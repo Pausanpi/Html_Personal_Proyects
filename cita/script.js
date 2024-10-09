89ba44b92f2e28ec422ed19a93b4d2b40aa19d18
Paula Sánchez <123211878+Pausanpi@users.noreply.github.com>
2024-10-09 16:56:24 +0200

    Add files via upload

--- cita/script.js
+++ cita/script.js
@@ -6,11 +6,16 @@ document.addEventListener('DOMContentLoaded', function () {
     const preguntaInicial = document.getElementById('preguntaInicial');
     const botonSi = document.getElementById('botonSi');
     const botonNo = document.getElementById('botonNo');
+	const container1 = document.querySelector(".container1");
+	const container2 = document.querySelector(".container2");
 
     // Mostrar el formulario al hacer clic en "Sí"
     botonSi.addEventListener('click', function() {
+		container1.style.display = "none";
+		container2.style.display = "block";
         preguntaInicial.style.display = 'none'; // Oculta la pregunta inicial
         form.style.display = 'block';           // Muestra el formulario
+		opcionesCita.style.display = 'block';
     });
 
 	botonNo.addEventListener('mouseenter', function() {
@@ -28,16 +33,35 @@ document.addEventListener('DOMContentLoaded', function () {
         // Recolectar los datos del formulario
         const nombre = document.getElementById('nombre').value;
         const fecha = document.getElementById('fecha').value;
+		const correo = document.getElementById('correo').value;
+
+		let opcionSeleccionada = '';
+		const opciones = document.getElementsByName('opcionCita');
+		for (let i = 0; i < opciones.length; i++) {
+			if (opciones[i].checked) {
+				opcionSeleccionada = opciones[i].value;
+				break;
+			}
+		}
 
         // Configuración del email a enviar
         const templateParams = {
             nombre: nombre,
             fecha: fecha,
-            correo: 'pausanpi1606@gmail.com' // Siempre se enviará a este correo
+            correo: 'pausanpi1606@gmail.com', // Siempre se enviará a este correo
+			opcion: opciones
+        };
+
+		const templateParams2 = {
+            nombre: nombre,
+            fecha: fecha,
+            user_correo: correo, // Siempre se enviará a este correo
+			opcion: opciones
         };
 
         // Enviar el correo a través de EmailJS
         emailjs.send('service_kkvm9re', 'template_by3cdj5', templateParams)
+		emailjs.send('service_kkvm9re', 'template_48byf3z', templateParams2)
             .then(function(response) {
                 console.log('SUCCESS!', response.status, response.text);
                 alert('Cita agendada con éxito!');
@@ -46,5 +70,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 console.error('Error al enviar el correo:', error);
                 alert('Error al agendar la cita. Inténtalo de nuevo.');
             });
+		
     });
 });
